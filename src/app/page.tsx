855bab9af29dd3bd230f46e6012a5ba35f2a315b
Idenilson Santos <idenilsonoliveira.santos@gmail.com>
2024-06-26 12:47:15 -0300

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar/>
+      <div className="max-w-4xl mx-auto px-4 sm:px-6">
+        <HeroSection />
+      </div>
     </main>
   );
 }
---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,64 @@
+"use client";
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const HeroSection = () => {
+  return (
+    <section className="pt-56" id="home">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+      >
+        <h1 className="text-white mb-4 text-2xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
+          <span className="text-teal-400">Olá, eu sou</span>
+          <br></br>
+          <TypeAnimation
+            sequence={[
+              "Idenilson",
+              1000,
+              "Dev Frontend",
+              1000,
+              "Dev Backend",
+              1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+          />
+        </h1>
+        <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+          Sou um desenvolvedor fullstack (React.js e Node.js) focado em
+          desenvolver soluções que permitam uma melhor experiência.
+          Eventualmente utilizo outras tecnolgias que melhor se apliquem ao
+          projeto que estou atuando no momento.
+        </p>
+        <div>
+          <Link
+            href="/#contact"
+            className="px-6 inline-block py-3 w-full sm:w-fit rounded-lg mr-4 bg-teal-600 hover:bg-teal-400 transition ease-in-out text-white"
+          >
+            Me contrate
+          </Link>
+          <Link
+            href="/"
+            className="px-1 inline-block py-1 w-full sm:w-fit rounded-lg hover:text-teal-800 transition ease-in-out text-white mt-3"
+          >
+            <span className="block rounded px-5 py-2">Download CV</span>
+          </Link>
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        className="col-span-4 place-self-center mt-4 lg:mt-0"
+      ></motion.div>
+    </section>
+  );
+};
+
+export default HeroSection;

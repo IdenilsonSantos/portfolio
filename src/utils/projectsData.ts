638a7e9bd013f4c7be4d203cd96582f6a5cfc7fb
Idenilson Santos <idenilsonoliveira.santos@gmail.com>
2024-08-11 17:55:45 -0300

    update: portfolio data

--- src/utils/projectsData.ts
+++ src/utils/projectsData.ts
@@ -1,27 +1,17 @@
 export const projectsData = [
   {
     id: 1,
-    title: "Discord Clone UI",
-    description: "Desenvolvimento de um clone da ui do Discord.",
+    title: "Barba Brutal",
+    description:
+      "App para agendamento de serviços em barbearia.",
     image:
-      "https://res.cloudinary.com/djgvgwuwe/image/upload/v1719165650/portfolio/notn1g1h7ykdwrf34ev0.png",
-    gitUrl: "/",
+      "https://res.cloudinary.com/djgvgwuwe/image/upload/v1723407970/portfolio/maagvb4xrhjy7codaakg.png",
+    gitUrl: "https://github.com/IdenilsonSantos/barbabrutal",
     previewUrl: "/",
-    tags: ["React", "Styled Js"],
+    tags: ["Turbo", "NextJs", "React Native", "NestJs", "Prisma"],
   },
   {
     id: 2,
-    title: "Be the Hero",
-    description:
-      "Criação de um local onde as ongs ou pessoas pudessem pedir ajuda.",
-    image:
-      "https://res.cloudinary.com/djgvgwuwe/image/upload/v1719165650/portfolio/jgsnxnjk7l0jn0koco3d.png",
-    gitUrl: "https://github.com/IdenilsonSantos/bethehero11",
-    previewUrl: "/",
-    tags: ["React", "Styled Js", "Express", "Node", "SQL DB"],
-  },
-  {
-    id: 3,
     title: "TMDB UI",
     description:
       "Uma nova visulização para o tmdb, site de busca de informações sobre filmes.",
@@ -32,8 +22,29 @@ export const projectsData = [
     tags: ["React", "Tailwind"],
   },
   {
+    id: 3,
+    title: "Discord Clone UI",
+    description: "Desenvolvimento de um clone da ui do Discord.",
+    image:
+      "https://res.cloudinary.com/djgvgwuwe/image/upload/v1719165650/portfolio/notn1g1h7ykdwrf34ev0.png",
+    gitUrl: "/",
+    previewUrl: "/",
+    tags: ["React", "Styled Js"],
+  },
+  {
     id: 4,
-    title: "Portfolio",
+    title: "Be the Hero",
+    description:
+      "Criação de um local onde as ongs ou pessoas pudessem pedir ajuda.",
+    image:
+      "https://res.cloudinary.com/djgvgwuwe/image/upload/v1719165650/portfolio/jgsnxnjk7l0jn0koco3d.png",
+    gitUrl: "https://github.com/IdenilsonSantos/bethehero11",
+    previewUrl: "/",
+    tags: ["React", "Styled Js", "Express", "Node", "SQL DB"],
+  },
+  {
+    id: 5,
+    title: "Portifólio",
     description:
       "Criação do portfolio próprio.",
     image:
@@ -41,5 +52,27 @@ export const projectsData = [
     gitUrl: "https://github.com/IdenilsonSantos/portfolio",
     previewUrl: "/",
     tags: ["React", "Tailwind", "NextJs", "Frame motion"],
+  },
+  {
+    id: 6,
+    title: "Union Voluntários",
+    description:
+      "Busca de locais que necessitem de trabalho voluntário.",
+    image:
+      "https://res.cloudinary.com/djgvgwuwe/image/upload/v1723407210/portfolio/dpbshssy0viscqrbqugw.png",
+    gitUrl: "https://github.com/IdenilsonSantos/union_voluntarios",
+    previewUrl: "/",
+    tags: ["React", "Typescript", "Nodejs", "Sqlite", "TypeOrm"],
+  },
+  {
+    id: 7,
+    title: "PDV Plus",
+    description:
+      "Visual estático de ponto de venda para comércios",
+    image:
+      "https://res.cloudinary.com/djgvgwuwe/image/upload/v1723304693/portfolio/sw7e7zdwhm3qnwl4esra.png",
+    gitUrl: "https://github.com/IdenilsonSantos/pdvplus",
+    previewUrl: "/",
+    tags: ["ReactJs", "Typescript", "Styled Components"],
   },
 ];
